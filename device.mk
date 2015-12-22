787c459738a9b7bc4e56bfc3707a010834d0bfb4
Tony Malagisi <mtb3000gt@gmail.com>
2015-12-22 10:38:56 -0500

    zerofltespr: Add RIL shim library
    Conflicts:
    	BoardConfig.mk

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.sec.boot.sh
 
+# Samsung symbols
+PRODUCT_PACKAGES += \
+    libsamsung_symbols
+
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     hostapd \
